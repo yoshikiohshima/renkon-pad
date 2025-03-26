dbe5cd66ceb3484f962d4b1517f7a9bc6fd71868
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2025-03-25 17:01:46 -0700

    remove test default string

--- pad.js
+++ pad.js
@@ -230,7 +230,7 @@ export function pad() {
     const newEditor = (id, doc) => {
         const mirror = window.CodeMirror;
         const editor = new mirror.EditorView({
-            doc: doc || `console.log("hello")\na\nb\nc\nd\ne\nf\ng\nh\ni\nk`,
+            doc: doc || `console.log("hello")`,
             extensions: [
                 mirror.basicSetup,
                 mirror.EditorView.lineWrapping,
