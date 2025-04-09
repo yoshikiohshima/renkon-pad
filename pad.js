426d461110828f3192d9aa2f3ec37acb86339c23
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2025-04-09 15:37:01 -0700

    include stringify functions

--- pad.js
+++ pad.js
@@ -2,7 +2,6 @@ export function pad() {
     // Initialization
 
     const {h, html, render} = import("./preact.standalone.module.js");
-    const {stringify, parse, stringifyCodeMap, parseCodeMap} = import ("./stable-stringify.js");
 
     const renkon = (() => {
         const renkon = document.createElement("div");
@@ -31,6 +30,102 @@ export function pad() {
         document.body.appendChild(renkon);
         return renkon;
     })();
+
+    // Stringify
+
+    /*
+  Copyright 2020 Croquet Corporation.
+  Copyright 2025 Yoshiki Ohshima
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+    function stringifyInner(node, seen) {
+        if (node === undefined) return undefined;
+        if (typeof node === 'number') return Number.isFinite(node) ? `${node}` : 'null';
+        if (typeof node !== 'object') return JSON.stringify(node, null, 4);
+
+        let out;
+        if (Array.isArray(node)) {
+            out = '[';
+            for (let i = 0; i < node.length; i++) {
+                if (i > 0) out += ',';
+                out += stringifyInner(node[i], seen) || 'null';
+            }
+            return out + ']';
+        }
+
+        if (node === null) return 'null';
+
+        if (seen.has(node)) {
+            throw new TypeError('Converting circular structure to JSON');
+        }
+
+        seen.add(node);
+
+        if (node.constructor === window.Map) {
+            let replacement = {__map: true, values: [...node]};
+            return stringifyInner(replacement, seen);
+        }
+
+        if (node.constructor === window.Set) {
+            let replacement = {__set: true, values: [...node]};
+            return stringifyInner(replacement, seen);
+        }
+
+        let keys = Object.keys(node).sort();
+        out = '';
+        for (let i = 0; i < keys.length; i++) {
+            let key = keys[i];
+            let value = stringifyInner(node[key], seen, out);
+            if (!value) continue;
+            if (out !== '') out += ',\n';
+            out += JSON.stringify(key) + ':' + value;
+        }
+        seen.delete(node);
+        return '{' + out + '}';
+    }
+
+    function stringify(obj) {
+        let seen = new Set();
+        return stringifyInner(obj, seen);
+    }
+
+    function parse(string) {
+        return JSON.parse(string, (_key, value) => {
+            if (typeof value === "object" && value !== null && value.__map) {
+                return new Map(value.values);
+            } else if (typeof value === "object" && value !== null && value.__set) {
+                return new Set(value.values);
+            }
+            return value;
+        });
+    }
+
+    function stringifyCodeMap(map) {
+
+        function replace(str) {
+            return str.replaceAll("\\", "\\\\").replaceAll("`", "\\`").replaceAll("$", "\\$");
+        }
+
+        return "\n{__codeMap: true, value: " + "[" +
+            [...map].map(([key, value]) => ("[" + "`" + replace(key) + "`" + ", " + "`" +  replace(value) + "`" + "]")).join(",\n") + "]" + "}"
+    }
+
+    function parseCodeMap(string) {
+        const array = eval("(" + string + ")");
+        return new Map(array.value);
+    }
 
     // Data Structure
 
@@ -250,6 +345,43 @@ export function pad() {
 
     const newWindowRequest = Events.change({id: newId, type: Events.or(addCode, addRunner, init), padView});
 
+    const padView = Behaviors.select(
+        {x: 0, y: 0, scale: 1},
+        padViewChange, (now, view) => {
+            let {x, y, scale} = view;
+
+            if (scale < 0.1) {scale = 0.1;}
+            if (scale > 20) {scale = 20;}
+            return {...now, ...{x, y, scale}};
+        }
+    );
+
+    const padTitle = Behaviors.select(
+        "untitled",
+        loadRequest, (now, data) => {
+            console.log("padTitle loaded");
+            return data.padTitle || "untitled"
+        },
+        titleChange, (now, request) => request
+    );
+
+    const titleChange = Events.observe((notify) => {
+        const change = (evt) => {
+            notify(evt.target.value);
+        };
+
+        renkon.querySelector("#padTitle").addEventListener("input", change);
+        return () => {renkon.querySelector("#padTitle").removeEventListener("change", change);}
+    });
+
+    const _padTitleUpdater = ((padTitle) => {
+        if (renkon.querySelector("#padTitle").value !== padTitle) {
+            renkon.querySelector("#padTitle").value = padTitle;
+        }
+    })(padTitle);
+
+    // New Component
+
     const newEditor = (id, doc) => {
         const mirror = window.CodeMirror;
 
@@ -271,10 +403,10 @@ export function pad() {
             if (!showDependency || showDependency !== "showDeps") {return;}
             let decls;
             try {
-              decls = Renkon.findDecls(state.doc.toString());
+                decls = Renkon.findDecls(state.doc.toString());
             } catch(e) {
-              console.log("Dependency analyzer encountered an error in source code:");
-              return;
+                console.log("Dependency analyzer encountered an error in source code:");
+                return;
             }
             const head = pos !== undefined ? pos : state.selection.ranges[0]?.head;
             if (typeof head !== "number") {return;}
@@ -396,41 +528,6 @@ export function pad() {
         runnerIframe.id = `runner-${id}`;
         return {dom: runnerIframe};
     };
-
-    const padView = Behaviors.select(
-        {x: 0, y: 0, scale: 1},
-        padViewChange, (now, view) => {
-            let {x, y, scale} = view;
-
-            if (scale < 0.1) {scale = 0.1;}
-            if (scale > 20) {scale = 20;}
-            return {...now, ...{x, y, scale}};
-        }
-    );
-
-    const padTitle = Behaviors.select(
-        "untitled",
-        loadRequest, (now, data) => {
-            console.log("padTitle loaded");
-            return data.padTitle || "untitled"
-        },
-        titleChange, (now, request) => request
-    );
-
-    const titleChange = Events.observe((notify) => {
-        const change = (evt) => {
-            notify(evt.target.value);
-        };
-
-        renkon.querySelector("#padTitle").addEventListener("input", change);
-        return () => {renkon.querySelector("#padTitle").removeEventListener("change", change);}
-    });
-
-    const _padTitleUpdater = ((padTitle) => {
-        if (renkon.querySelector("#padTitle").value !== padTitle) {
-            renkon.querySelector("#padTitle").value = padTitle;
-        }
-    })(padTitle);
 
     // User Interaction
 
